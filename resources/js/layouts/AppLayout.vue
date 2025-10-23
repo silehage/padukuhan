0ef779e553fbd4e02584d9d3274214ca9591a5d9
silehage <silehwage@gmail.com>
2025-10-24 01:25:58 +0700

    fix drawer

--- resources/js/layouts/AppLayout.vue
+++ resources/js/layouts/AppLayout.vue
@@ -10,7 +10,7 @@
             <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
           </q-avatar> -->
           Administrasi RT 05
-          
+
         </q-toolbar-title>
         <q-btn icon="logout" flat round @click="router.visit(logout())">
           <q-tooltip>Logout</q-tooltip>
@@ -19,18 +19,19 @@
     </q-header>
 
     <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered :mini="mini" :width="210">
-     <q-list class="q-py-md">
-       <q-item clickable  v-for="(nav, idx) in navItems" :key="idx" @click="router.visit(nav.href)">
-         <q-item-section side>
-           <q-icon :name="nav.icon"></q-icon>
+      <q-list class="q-py-md">
+        <q-item clickable v-for="(nav, idx) in navItems" :key="idx" @click="handleClickMenu(nav.href)">
+          <q-item-section side>
+            <q-icon :name="nav.icon"></q-icon>
           </q-item-section>
           <q-item-section>
             <q-item-label>{{ nav.title }}</q-item-label>
           </q-item-section>
-          <q-tooltip v-if="mini" anchor="center right" self="center left" :offset="[10, 10]" class="bg-purple text-white">{{ nav.title }}</q-tooltip>
-        
-       </q-item>
-     </q-list>
+          <q-tooltip v-if="mini" anchor="center right" self="center left" :offset="[10, 10]"
+            class="bg-purple text-white">{{ nav.title }}</q-tooltip>
+
+        </q-item>
+      </q-list>
     </q-drawer>
 
     <q-page-container>
@@ -50,6 +51,9 @@ import { router } from '@inertiajs/vue3';
 import inventaris from '@/routes/inventaris';
 import kas from '@/routes/kas';
 import { logout } from '@/routes';
+import { useQuasar } from 'quasar';
+
+const $q = useQuasar()
 
 defineProps<{
   title?: string;
@@ -60,12 +64,19 @@ const mini = ref(false)
 
 const toggleLeftDrawer = () => {
 
-  if (window.innerWidth > 1000) {
+  if ($q.platform.is.desktop) {
     leftDrawerOpen.value = true
     mini.value = !mini.value
   } else {
     leftDrawerOpen.value = !leftDrawerOpen.value
   }
+}
+
+const handleClickMenu = (href) => {
+  if ($q.platform.is.mobile) {
+    leftDrawerOpen.value = !leftDrawerOpen.value
+  }
+  router.visit(href)
 }
 
 const navItems = [
